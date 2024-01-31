889c6a30a2e63e344a285315999580de12c74c28
Daniel Amas <amasdaniel3@gmail.com>
2024-01-31 16:35:44 -0500

    fixed errors in q1

--- tut3/question1.c
+++ tut3/question1.c
@@ -4,15 +4,17 @@
 int main(void)
 {
     int age;
-    char* firstName[32];
+    char firstName[32];
     float height;
 
     printf("Input your first name: ");
-    fgets(&firstName, 32, stdin);
-    printf("Input your age: ");
-    scanf("%d\n", age);
-    printf("Input your height: ");
-    scanf("%f\n", height);
+    fgets(firstName, sizeof(firstName), stdin);
 
-    printf("Your name is %s\nYour age is %d\nYour height is %f", firstName, age, height);
+    printf("Input your age: ");
+    scanf("%d", &age);
+    
+    printf("Input your height: ");
+    scanf("%f", &height);
+
+    printf("Your name is %sYour age is %d\nYour height is %.1f", firstName, age, height);
 }
