ddd71ae1bf379f38822db7dae4ebc0b9f2150768
DanielAmas <danielamas@Linux22.04daniel>
2024-01-31 16:25:27 -0500

    finish question1

--- tut3/question1.c
+++ tut3/question1.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
+    int age;
+    char* firstName[32];
+    float height;
 
+    printf("Input your first name: ");
+    fgets(&firstName, 32, stdin);
+    printf("Input your age: ");
+    scanf("%d\n", age);
+    printf("Input your height: ");
+    scanf("%f\n", height);
+
+    printf("Your name is %s\nYour age is %d\nYour height is %f", firstName, age, height);
 }
